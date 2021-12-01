b94b8ccd5e9c9a2c26aeca1ece611933e0a5d740
Scott C <Scanfiel@uccs.edu>
2021-12-01 19:14:10 +0000

    Added User Tests for Rspec

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -5,6 +5,8 @@ RSpec.feature "Projects", type: :feature do   #This line specifically looking fo
   
   context "Create new project" do
     before(:each) do
+      user = FactoryBot.create(:user)
+      login_as(user)
       visit new_project_path
       within("form") do
         fill_in "Title", with: "Test title"
@@ -30,6 +32,8 @@ RSpec.feature "Projects", type: :feature do   #This line specifically looking fo
   context "Update project" do
     let(:project) { Project.create(title: "Test title", description: "Test content") }
     before(:each) do
+      user = FactoryBot.create(:user)
+      login_as(user)
       visit edit_project_path(project)
     end
 
@@ -56,6 +60,8 @@ RSpec.feature "Projects", type: :feature do   #This line specifically looking fo
   context "Remove existing project" do
     let!(:project) { Project.create(title: "Test title", description: "Test content") }
     scenario "remove project" do
+      user = FactoryBot.create(:user)
+      login_as(user)
       visit projects_path
       click_link "Destroy"
       expect(page).to have_content("Project was successfully destroyed")
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,29 +3,40 @@ require 'devise'
 
 RSpec.describe User, type: :model do
   
-  before(:all) do
-    @user1 = create(:user)
+  describe 'user registration' do 
+    it "Account Created Successful" do 
+      user = build(:user) 
+      expect(user).to be_valid # user.valid?Pass if is true end
+    end
   end
-  
-  it "is valid with valid attributes" do
-    expect(@user1).to be_valid
-  end
-  
-  it "has a unique email" do
-    user2 = build(:user, email: "test@user.com")
-    expect(user2).to_not be_valid
-  end
-  
-  
-  it "is not valid without a password" do 
-    user2 = build(:user, password: nil)
-    expect(user2).to_not be_valid
-  end
-  
-  
-  it "is not valid without an email" do
-    user2 = build(:user, email: nil)
-    expect(user2).to_not be_valid
-  end  
-end
 
+  describe 'password changed successful' do 
+    it "pw successful" do 
+      user = build(:user) 
+      expect(user).to be_valid # user.valid?Pass if is true end
+    end
+  end
+
+  describe 'user registration' do 
+    it "Account Created Successful" do 
+      user = build(:user) 
+      expect(user).to be_valid # user.valid?Pass if is true end
+    end
+  end
+
+  describe 'user registration' do 
+    it "Account Created Successful" do 
+      user = build(:user) 
+      expect(user).to be_valid # user.valid?Pass if is true end
+    end
+  end
+
+  #describe 'unique email' do
+   # it 'should not be valid' do
+    #  user = build(:user)
+     # user2 = build(:user)
+      #expect(user).to_not be_valid
+    #end
+  #end
+
+end
