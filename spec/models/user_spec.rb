4883a42fac406636d1853e29e834400d0355ef3b
Scott C <Scanfiel@uccs.edu>
2021-12-01 19:30:47 +0000

    Nearly finished with Rspec Testing for User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,38 +5,41 @@ RSpec.describe User, type: :model do
   
   describe 'user registration' do 
     it "Account Created Successful" do 
-      user = build(:user) 
+      user = create(:user) 
       expect(user).to be_valid # user.valid?Pass if is true end
     end
   end
 
   describe 'password changed successful' do 
     it "pw successful" do 
-      user = build(:user) 
-      expect(user).to be_valid # user.valid?Pass if is true end
+      user = create(:user)
+      login_as(user)
+      visit edit_user_password_path
+      fill_in 'New password', with: 'p4ssw0rd'
+      fill_in 'Confirm new password', with: 'p4ssw0rd'
+      click_button 'Change my password'
+      expect(page).to have_text 'Your password has been changed successfully.'
     end
   end
 
-  describe 'user registration' do 
-    it "Account Created Successful" do 
-      user = build(:user) 
-      expect(user).to be_valid # user.valid?Pass if is true end
-    end
+  scenario 'with invalid data' do
+    visit user_registration
+    click_button 'Sign up'
+    expect(page).to have_text "Email can't be blank"
+    expect(page).to have_text "Password can't be blank"
+    expect(page).to have_no_link 'Sign Out'
   end
 
-  describe 'user registration' do 
-    it "Account Created Successful" do 
-      user = build(:user) 
-      expect(user).to be_valid # user.valid?Pass if is true end
+  feature 'User signs out' do
+
+    scenario 'user signed in' do
+      user = create :user
+      sign_in user
+      visit root_path
+      click_link 'Logout'
+      expect(page).to have_text 'Signed out successfully.'
+      expect(page).to have_no_link 'Logout'
+      expect(page).to have_current_path root_path
     end
   end
-
-  #describe 'unique email' do
-   # it 'should not be valid' do
-    #  user = build(:user)
-     # user2 = build(:user)
-      #expect(user).to_not be_valid
-    #end
-  #end
-
 end
