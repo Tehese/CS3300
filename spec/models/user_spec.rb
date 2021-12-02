388312991cde81c2d5511da81107607026ce2c00
Your Name <Scanfiel@uccs.edu>
2021-12-02 04:16:18 +0000

    Finished/Unfinished User Rspec Tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,14 @@ RSpec.describe User, type: :model do
   
   describe 'user registration' do 
     it "name, email, password and password_You can register if confirmation exists" do
-      user = build(:user) 
+      user = build(:user)
       expect(user).to be_valid # user.valid?Pass if is true 
     end
   end
 end
+
+
+
+#Create a check for all these
+#database_authenticatable, :registerable,
+#        :recoverable, :rememberable, :validatable
