91b8552b5e9226ac0d3116fc46cbcfa1f720ff68
Your Name <Scanfiel@uccs.edu>
2021-12-02 02:18:23 +0000

    Added onto the last one

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -21,4 +21,16 @@ RSpec.describe ProjectsController, type: :controller do
       expect(response).to be_success
     end
   end
+
+  it "should have a current_user" do
+    # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
+    expect(subject.current_user).to_not eq(nil)
+  end
+
+  it "should get index" do
+    # Note, rails 3.x scaffolding may add lines like get :index, {}, valid_session
+    # the valid_session overrides the devise login. Remove the valid_session from your specs
+    get 'index'
+    response.should be_success
+  end
 end
