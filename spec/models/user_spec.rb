486b692a0faec3c1fabbe550195c65256edae01d
Your Name <Scanfiel@uccs.edu>
2021-12-06 21:44:18 +0000

    Updated User Testing and What it shows to users logged in/not

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,24 @@ require "rails_helper"
 require_relative "../support/controller_macros"
 
 
-RSpec.describe User, type: :model do
-  describe 'user registration' do 
-    it "name, email, password and password_You can register if confirmation exists" do
-      user = build(:user)
-      expect(user).to be_valid # user.valid?Pass if is true 
+RSpec.describe 'User Model Testing', type: :model do
+  
+  before do
+    @user = build(:user)
+  end
+
+  describe 'Require Email' do
+    it 'Fails if empty' do
+      @user.email = ''
+      expect(@user.valid?).to eq(false)
+    end
+  end
+
+  describe 'Require Password' do
+    it 'Fails if empty' do
+      @user.password = ''
+      expect(@user.valid?).to eq(false)
     end
   end
 end
 
-
-
-#Create a check for all these
-#database_authenticatable, :registerable,
-#        :recoverable, :rememberable, :validatable
